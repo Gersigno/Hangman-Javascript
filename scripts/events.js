49c9ef2a4a4ad3e26d305f9f11fae243e2ccfd1d
Gersigno <contact.gersigno@gmail.com>
2025-01-23 16:33:57 +0100

    Variables type update & indentation fix

--- scripts/events.js
+++ scripts/events.js
@@ -3,8 +3,8 @@
  * it will trigger the "InitGame()" function
  */
 function Event_Loaded() {
-    //when the page is loaded, we can initialize the game..
-    InitGame();
+	//when the page is loaded, we can initialize the game..
+	InitGame();
 }
 
 /**
@@ -12,32 +12,32 @@ function Event_Loaded() {
  */
 function Event_newWord() {
 
-    updateKeyboard();
+	updateKeyboard();
 
-    wordToGuess = "";
-    revealedLetters = [];
-    tries = 0;
-    var element_keyboard = document.getElementById("VirtualKeyboard");
-    var element_message = document.getElementById("message");
+	wordToGuess = "";
+	revealedLetters = [];
+	tries = 0;
+	const element_keyboard = document.getElementById("VirtualKeyboard");
+	const element_message = document.getElementById("message");
 
-    root.style.setProperty('--hangman-src', "url(\"../resources/p0.png\")");
-    root.style.setProperty('--background-color', "rgb(227,227,227)");
-    element_keyboard.classList.remove("KeyboardDisabled");
-    element_keyboard.classList.add("KeyboardEnabled");
+	root.style.setProperty('--hangman-src', "url(\"../resources/p0.png\")");
+	root.style.setProperty('--background-color', "rgb(227,227,227)");
+	element_keyboard.classList.remove("KeyboardDisabled");
+	element_keyboard.classList.add("KeyboardEnabled");
 
-    InitGame();
+	InitGame();
 
-    element_message.innerHTML = "";
-    element_message.classList.remove("MessageVisible");
-    element_message.classList.add("MessageHidden");
+	element_message.innerHTML = "";
+	element_message.classList.remove("MessageVisible");
+	element_message.classList.add("MessageHidden");
 }
 
 /**
  * Adjust the game's difficulty by selecting the value of the difficulty drop down menu.
  */
 function Event_UpdateDifficulty() {
-    difficulty = document.getElementById("difficulty").value;
-    Event_newWord();
+	difficulty = document.getElementById("difficulty").value;
+	Event_newWord();
 }
 
 /**
@@ -45,40 +45,40 @@ function Event_UpdateDifficulty() {
  * @param {*} letter button's letter (pass it as an "this" reference from the onclick event)
  */
 function Event_GuessNewLetter(letter) {
-    //First, obtain the inner letter of our button..
-    var targetLetter = letter.innerHTML;
-    //then, push it into our revealed letters array.
-    revealedLetters.push(targetLetter);
-    UpdateUI();
-    isLetterInWord(targetLetter)
-    updateKeyboard();
+	//First, obtain the inner letter of our button..
+	const targetLetter = letter.innerHTML;
+	//then, push it into our revealed letters array.
+	revealedLetters.push(targetLetter);
+	UpdateUI();
+	isLetterInWord(targetLetter)
+	updateKeyboard();
 }
 
 /**
  * Event called when the player loses/runs out of attempts.
  */
 function Event_Lose() {
-    var element_keyboard = document.getElementById("VirtualKeyboard");
-    var element_message = document.getElementById("message");
+	const element_keyboard = document.getElementById("VirtualKeyboard");
+	const element_message = document.getElementById("message");
 
-    element_message.innerHTML = "Perdu ! <br>Le mot était : <b>" + wordToGuess + "</b><br>Clique ici pour re-jouer";
-    element_keyboard.classList.remove("KeyboardEnabled");
-    element_keyboard.classList.add("KeyboardDisabled");
-    element_message.classList.remove("MessageHidden");
-    element_message.classList.add("MessageVisible");
+	element_message.innerHTML = "Perdu ! <br>Le mot était : <b>" + wordToGuess + "</b><br>Clique ici pour re-jouer";
+	element_keyboard.classList.remove("KeyboardEnabled");
+	element_keyboard.classList.add("KeyboardDisabled");
+	element_message.classList.remove("MessageHidden");
+	element_message.classList.add("MessageVisible");
 }
 
 /**
  * Event called when the player has guessed successfully our secret word
  */
 function Event_Won() {
-    var element_keyboard = document.getElementById("VirtualKeyboard");
-    var element_message = document.getElementById("message");
+	const element_keyboard = document.getElementById("VirtualKeyboard");
+	const element_message = document.getElementById("message");
 
-    element_message.innerHTML = "Bien joué ! <br>Tu as trouvé en <b>"+tries+"</b> essais!<br>Clique ici pour re-jouer";
-    root.style.setProperty('--background-color', "rgb(0,125,0)");
-    element_keyboard.classList.remove("KeyboardEnabled");
-    element_keyboard.classList.add("KeyboardDisabled");
-    element_message.classList.remove("MessageHidden");
-    element_message.classList.add("MessageVisible");
+	element_message.innerHTML = "Bien joué ! <br>Tu as trouvé en <b>" + tries + "</b> essais!<br>Clique ici pour re-jouer";
+	root.style.setProperty('--background-color', "rgb(0,125,0)");
+	element_keyboard.classList.remove("KeyboardEnabled");
+	element_keyboard.classList.add("KeyboardDisabled");
+	element_message.classList.remove("MessageHidden");
+	element_message.classList.add("MessageVisible");
 }
